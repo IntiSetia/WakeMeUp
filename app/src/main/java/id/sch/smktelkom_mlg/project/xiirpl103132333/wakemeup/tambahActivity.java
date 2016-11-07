e83ed5b8257d113ef65e6018ad6190fb0c3b62dd
ASR <karepmuron@gmail.com>
2016-11-07 22:38:11 +0700

    Penambahan script TimePickerDialog

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl103132333/wakemeup/tambahActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl103132333/wakemeup/tambahActivity.java
@@ -1,13 +1,86 @@
 package id.sch.smktelkom_mlg.project.xiirpl103132333.wakemeup;
 
+import android.app.AlarmManager;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class tambahActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class tambahactivity extends AppCompatActivity {
+
+    static final int TIME_DIALOG_ID = 1;
+    // variables to save user selected date and time
+    public int hour, minute;
+    Button btnWaktu;
+    EditText etWaktu;
+    TimePicker alarm_timepicker;
+    AlarmManager alarm_manager;
+    Context context;
+    // declare  the variables to Show/Set the date and time when Time and  Date Picker Dialog first appears
+    private int mHour, mMinute;
+    // Register  TimePickerDialog listener
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        // the callback received when the user "sets" the TimePickerDialog in the dialog
+        public void onTimeSet(TimePicker view, int hourOfDay, int min) {
+            hour = hourOfDay;
+            minute = min;
+            // Set the Selected Date in Select date Button
+            etWaktu.setText(hour + ":" + minute);
+        }
+    };
+
+    public tambahactivity() {
+        // Assign current Date and Time Values to Variables
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah);
+        btnWaktu = (Button) findViewById(R.id.buttonWaktu);
+        etWaktu = (EditText) findViewById(R.id.editTextWaktu);
+
+        // Set ClickListener on btnSelectTime
+        btnWaktu.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // Show the TimePickerDialog
+                showDialog(TIME_DIALOG_ID);
+            }
+        });
+
+        findViewById(R.id.buttonSet).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String a = etWaktu.getText().toString();
+                Toast.makeText(getApplicationContext(), "Alarm Akan Berbunyi pada jam " + a, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(tambahactivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            // create a new TimePickerDialog with values you want to show
+            case TIME_DIALOG_ID:
+                return new TimePickerDialog(this, mTimeSetListener, mHour, mMinute, false);
+
+        }
+        return null;
+    }
+
 }
